f67854c695b51b3af5785a2e5d649b625a65b0b8
Nugroho Dewantoro <nugroho8dewantoro@gmail.com>
2023-12-12 06:36:05 +0700

    fix compile

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@ glslc src/shader/forward.frag -o build/shader/forward.frag.spv
 glslc src/shader/forward.vert -o build/shader/forward.vert.spv
 glslc src/shader/deferred.frag -o build/shader/deferred.frag.spv
 glslc src/shader/deferred.vert -o build/shader/deferred.vert.spv
-glslc src/shader/shadow_map.frag -o build/shader/shadow_map.frag.spv
-glslc src/shader/shadow_map.geom -o build/shader/shadow_map.geom.spv
-glslc src/shader/shadow_map.vert -o build/shader/shadow_map.vert.spv
+glslc src/shader/point_shadow_map.geom -o build/shader/point_shadow_map.geom.spv
+glslc src/shader/point_shadow_map.vert -o build/shader/point_shadow_map.vert.spv
+glslc src/shader/spot_shadow_map.vert -o build/shader/spot_shadow_map.vert.spv
 
