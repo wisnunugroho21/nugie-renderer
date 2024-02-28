eb406db1a0fd10d9a475a1403cefd9fd260071e7
Nugroho Dewantoro <nugroho8dewantoro@gmail.com>
2024-02-29 03:46:13 +0700

    add terrain shader in compile.sh

--- compile.sh
+++ compile.sh
@@ -3,3 +3,5 @@ mkdir -p build/shader
 glslc src/shader/forward.frag -o build/shader/forward.frag.spv
 glslc src/shader/forward.vert -o build/shader/forward.vert.spv
 glslc src/shader/shadow_map.vert -o build/shader/shadow_map.vert.spv
+glslc src/shader/terrain.frag -o build/shader/terrain.frag.spv
+glslc src/shader/terrain.vert -o build/shader/terrain.vert.spv
