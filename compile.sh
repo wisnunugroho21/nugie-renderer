6cd707e4090f95ec077af1892c7420075bf7bd34
Nugroho Dewantoro <nugroho8dewantoro@gmail.com>
2024-05-24 15:08:58 +0700

    fix bug

--- compile.sh
+++ compile.sh
@@ -1,3 +1,8 @@
 mkdir -p build/shader
 
+glslc src/shader/forward.vert -o build/shader/forward.vert.spv
+glslc src/shader/forward.frag -o build/shader/forward.frag.spv
+glslc src/shader/ray_gen.comp -o build/shader/ray_gen.comp.spv
+glslc src/shader/ray_intersect.comp -o build/shader/ray_intersect.comp.spv
+glslc src/shader/ray_hit.comp -o build/shader/ray_hit.comp.spv
 glslc src/shader/sampling.comp -o build/shader/sampling.comp.spv
