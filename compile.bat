9bc87ae9f361b7762ca2166421decfb81492e71f
Nugroho Dewantoro <nugroho8dewantoro@gmail.com>
2024-06-19 06:38:03 +0700

    add better cube mesh shader

--- compile.bat
+++ compile.bat
@@ -2,3 +2,4 @@ mkdir build\shader
 
 glslc src/shader/mesh_shade.frag -o build/shader/mesh_shade.frag.spv --target-spv=spv1.4
 glslc src/shader/cube.mesh -o build/shader/cube.mesh.spv --target-spv=spv1.4
+glslc src/shader/better_cube.mesh -o build/shader/better_cube.mesh.spv --target-spv=spv1.4
