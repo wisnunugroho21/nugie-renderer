9ace984bb70ff79cbc8b5a7ba4b0a4579dca54af
Nugroho Dewantoro <nugroho8dewantoro@gmail.com>
2024-07-14 11:42:11 +0700

    update camera class

--- src/app/camera/camera.cpp
+++ src/app/camera/camera.cpp
@@ -6,25 +6,90 @@
 namespace NugieApp {
     Camera::Camera(uint32_t width, uint32_t height) : width{width}, height{height} {}
 
+    void Camera::setAspect(float aspectRatio) {
+		this->aspectRatio = aspectRatio;
+
+		assert(glm::abs(aspectRatio - std::numeric_limits<float>::epsilon()) > 0.0f);
+		const float tanHalfFovy = tan(this->fovy / 2.0f);
+		this->projectionMatrix = glm::mat4{0.0f};
+		this->projectionMatrix[0][0] = 1.f / (this->aspectRatio * tanHalfFovy);
+		this->projectionMatrix[1][1] = 1.f / (tanHalfFovy);
+		this->projectionMatrix[2][2] = this->far / (this->far - this->near);
+		this->projectionMatrix[2][3] = 1.f;
+		this->projectionMatrix[3][2] = -(this->far * this->near) / (this->far - this->near);
+	}
+
+    void Camera::setOrthographicProjection(float left, float right, float top, float bottom, float near, float far) {
+        this->projectionMatrix = glm::mat4{1.0f};
+		this->projectionMatrix[0][0] = 2.0f / (right - left);
+		this->projectionMatrix[1][1] = 2.0f / (bottom - top);
+		this->projectionMatrix[2][2] = 1.0f / (far - near);
+		this->projectionMatrix[3][0] = -(right + left) / (right - left);
+		this->projectionMatrix[3][1] = -(bottom + top) / (bottom - top);
+		this->projectionMatrix[3][2] = -near / (far - near);
+    }
+
+    void Camera::setPerspectiveProjection(float fovy, float aspectRatio, float near, float far) {
+        this->fovy = fovy;
+		this->aspectRatio = aspectRatio;
+		this->near = near;
+		this->far = far;
+
+		assert(glm::abs(aspectRatio - std::numeric_limits<float>::epsilon()) > 0.0f);
+		const float tanHalfFovy = tan(fovy / 2.0f);
+		this->projectionMatrix = glm::mat4{0.0f};
+		this->projectionMatrix[0][0] = 1.f / (aspectRatio * tanHalfFovy);
+		this->projectionMatrix[1][1] = 1.f / (tanHalfFovy);
+		this->projectionMatrix[2][2] = far / (far - near);
+		this->projectionMatrix[2][3] = 1.f;
+		this->projectionMatrix[3][2] = -(far * near) / (far - near);
+    }
+
     void Camera::setViewDirection(glm::vec3 position, glm::vec3 direction, float vfov, glm::vec3 vup) {
-        this->setViewTarget(position, direction + position, vfov, vup);
-    }
+        this->position = position;
+		this->direction = glm::normalize(direction);
 
-    void Camera::setViewTransformation(CameraTransformation cameraTransf, float vfov, glm::vec3 vup) {
-        this->setViewTarget(cameraTransf.origin, cameraTransf.origin + cameraTransf.direction, vfov, vup);
-    }
+		this->rotation.x = glm::acos(this->direction.z);
+		this->rotation.y = glm::atan(this->direction.y / this->direction.x);
 
-    void Camera::setViewTarget(glm::vec3 position, glm::vec3 target, float vfov, glm::vec3 vup) {
+		const glm::vec3 w{this->direction};
+		const glm::vec3 u{glm::normalize(glm::cross(w, vup))};
+		const glm::vec3 v{glm::cross(w, u)};
+
+		this->viewMatrix = glm::mat4{1.0f};
+		this->viewMatrix[0][0] = u.x;
+		this->viewMatrix[1][0] = u.y;
+		this->viewMatrix[2][0] = u.z;
+		this->viewMatrix[0][1] = v.x;
+		this->viewMatrix[1][1] = v.y;
+		this->viewMatrix[2][1] = v.z;
+		this->viewMatrix[0][2] = w.x;
+		this->viewMatrix[1][2] = w.y;
+		this->viewMatrix[2][2] = w.z;
+		this->viewMatrix[3][0] = -glm::dot(u, this->position);
+		this->viewMatrix[3][1] = -glm::dot(v, this->position);
+		this->viewMatrix[3][2] = -glm::dot(w, this->position);
+
+		this->inverseViewMatrix = glm::mat4{1.0f};
+		this->inverseViewMatrix[0][0] = u.x;
+		this->inverseViewMatrix[0][1] = u.y;
+		this->inverseViewMatrix[0][2] = u.z;
+		this->inverseViewMatrix[1][0] = v.x;
+		this->inverseViewMatrix[1][1] = v.y;
+		this->inverseViewMatrix[1][2] = v.z;
+		this->inverseViewMatrix[2][0] = w.x;
+		this->inverseViewMatrix[2][1] = w.y;
+		this->inverseViewMatrix[2][2] = w.z;
+		this->inverseViewMatrix[3][0] = this->position.x;
+		this->inverseViewMatrix[3][1] = this->position.y;
+		this->inverseViewMatrix[3][2] = this->position.z;
+
         float aspectRatio = static_cast<float>(this->width) / static_cast<float>(this->height);
 
         float theta = glm::radians(vfov);
         float h = glm::tan(theta / 2.0f);
         float viewportHeight = 2.0f * h;
         float viewportWidth = aspectRatio * viewportHeight;
-
-        glm::vec3 w = glm::normalize(position - target);
-        glm::vec3 u = glm::normalize(glm::cross(vup, w));
-        glm::vec3 v = glm::cross(w, u);
 
         this->cameraRay.origin = position;
         this->cameraRay.horizontal = glm::vec3(viewportWidth * u);
@@ -33,6 +98,14 @@ namespace NugieApp {
                 position - viewportWidth * u / 2.0f + viewportHeight * v / 2.0f - w);
 
         this->cameraTransformation.origin = position;
-        this->cameraTransformation.direction = target - position;
+        this->cameraTransformation.direction = direction;
+    }
+
+    void Camera::setViewTransformation(CameraTransformation cameraTransf, float vfov, glm::vec3 vup) {
+        this->setViewDirection(cameraTransf.origin, cameraTransf.direction, vfov, vup);
+    }
+
+    void Camera::setViewTarget(glm::vec3 position, glm::vec3 target, float vfov, glm::vec3 vup) {
+        this->setViewDirection(position, target - direction, vfov, vup);
     }
 } // namespace nugiEngine
--- src/app/camera/camera.hpp
+++ src/app/camera/camera.hpp
@@ -22,23 +22,43 @@ namespace NugieApp {
     public:
         Camera(uint32_t width, uint32_t height);
 
+        CameraRay getCameraRay() const { return this->cameraRay; }
+
+        CameraTransformation getCameraTransformation() const { return this->cameraTransformation; }
+
+        glm::vec3 getPosition() const { return this->position; }
+
+        glm::vec3 getDirection() const { return this->direction; }
+
+        glm::vec2 getRotation() const { return this->rotation; }
+
+        void setAspect(float aspectRatio);
+
+        void setOrthographicProjection(float left, float right, float top, float bottom, float near, float far);
+        void setPerspectiveProjection(float fovy, float aspectRatio, float near, float far);
+
         void setViewDirection(glm::vec3 position, glm::vec3 direction, float vfov,
                               glm::vec3 vup = glm::vec3{0.0f, 1.0f, 0.0f});
 
         void setViewTransformation(CameraTransformation cameraTransformation, float vfov,
                                    glm::vec3 vup = glm::vec3{0.0f, 1.0f, 0.0f});
 
-        void
-        setViewTarget(glm::vec3 position, glm::vec3 target, float vfov, glm::vec3 vup = glm::vec3{0.0f, 1.0f, 0.0f});
-
-        CameraRay getCameraRay() const { return this->cameraRay; }
-
-        CameraTransformation getCameraTransformation() const { return this->cameraTransformation; }
+        void setViewTarget(glm::vec3 position, glm::vec3 target, float vfov, 
+                           glm::vec3 vup = glm::vec3{0.0f, 1.0f, 0.0f});        
 
     private:
         CameraRay cameraRay;
         CameraTransformation cameraTransformation;
 
         uint32_t width, height;
+
+        glm::mat4 projectionMatrix{1.0f};
+        glm::mat4 viewMatrix{1.0f};
+        glm::mat4 inverseViewMatrix{1.0f};
+
+        glm::vec3 position, direction;
+        glm::vec2 rotation;
+
+        float fovy, aspectRatio, near, far;
     };
 } // namespace nugiEngine
