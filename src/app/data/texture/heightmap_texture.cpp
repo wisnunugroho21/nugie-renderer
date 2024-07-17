b93437b4dd96190c271a187246979bf7d3d625de
Nugroho Dewantoro <nugroho8dewantoro@gmail.com>
2024-07-18 05:59:01 +0700

    fix pipeline barrier

--- src/app/data/texture/heightmap_texture.cpp
+++ src/app/data/texture/heightmap_texture.cpp
@@ -31,15 +31,15 @@ namespace NugieApp {
 
         this->image->transitionImageLayout(commandBuffer, 
                                            VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 
-                                           VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT, 
+                                           VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT, 
                                            0, VK_ACCESS_TRANSFER_WRITE_BIT);
         
         this->stagingBuffer->copyBufferToImage(commandBuffer, this->image);
 
         this->image->transitionImageLayout(commandBuffer, 
                                            VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL, 
-                                           VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_MESH_SHADER_BIT_EXT, 
-                                           VK_ACCESS_TRANSFER_WRITE_BIT, VK_ACCESS_SHADER_READ_BIT);
+                                           VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT, 
+                                           VK_ACCESS_TRANSFER_WRITE_BIT, 0);
     }
 
     void HeightMapTexture::createSampler() {
