1d616b5142b2daf0f846a46027661dcf5cb4dc87
Nugroho Dewantoro <nugroho8dewantoro@gmail.com>
2024-07-17 17:40:49 +0700

    change file

--- compile_mesh_terrain.bat
+++ compile_mesh_terrain.bat
@@ -1,3 +1,3 @@
-glslc src/shader/mesh_shader/mesh_terrain.task -o build/shader/mesh_terrain.task.spv --target-spv=spv1.4
-glslc src/shader/mesh_shader/mesh_terrain.mesh -o build/shader/mesh_terrain.mesh.spv --target-spv=spv1.4
+glslc src/shader/mesh_shader/mesh_terrain_culling.task -o build/shader/mesh_terrain_culling.task.spv --target-spv=spv1.4
+glslc src/shader/mesh_shader/mesh_terrain_culling.mesh -o build/shader/mesh_terrain_culling.mesh.spv --target-spv=spv1.4
 glslc src/shader/mesh_shader/mesh_shade.frag -o build/shader/mesh_shade.frag.spv --target-spv=spv1.4
