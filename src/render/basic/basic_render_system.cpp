819fceafde265dbe468bafadc766e372254fae8f
Nugroho Dewantoro <nugroho8dewantoro@gmail.com>
2025-01-07 02:37:16 +0700

    delete all comments

--- src/render/basic/basic_render_system.cpp
+++ src/render/basic/basic_render_system.cpp
@@ -16,65 +16,28 @@ namespace nugie {
             }
         )";
 
-        wgpu::ShaderModuleWGSLDescriptor shaderCodeDesc;
-        // Set the chained struct's header
+        wgpu::ShaderModuleWGSLDescriptor shaderCodeDesc{};
         shaderCodeDesc.chain.next = nullptr;
         shaderCodeDesc.chain.sType = wgpu::SType::ShaderModuleWGSLDescriptor;
         shaderCodeDesc.code = shaderSource;    
 
-        wgpu::ShaderModuleDescriptor shaderDesc;
+        wgpu::ShaderModuleDescriptor shaderDesc{};
         shaderDesc.nextInChain = &shaderCodeDesc.chain;
 
         wgpu::ShaderModule shaderModule = device->createShaderModule(shaderDesc);
 
-        wgpu::RenderPipelineDescriptor pipelineDesc{};
-
-        // We do not use any vertex buffer for this first simplistic example
-        wgpu::VertexAttribute positionAttrib;
-        // == For each attribute, describe its layout, i.e., how to interpret the raw data ==
-        // Corresponds to @location(...)
+        wgpu::VertexAttribute positionAttrib{};
         positionAttrib.shaderLocation = 0;
-        // Means vec2f in the shader
         positionAttrib.format = wgpu::VertexFormat::Float32x2;
-        // Index of the first element
         positionAttrib.offset = 0;
 
-        wgpu::VertexBufferLayout vertexBufferLayout;
+        wgpu::VertexBufferLayout vertexBufferLayout{};
         vertexBufferLayout.attributeCount = 1;
         vertexBufferLayout.attributes = &positionAttrib;
-        // == Common to attributes from the same buffer ==
         vertexBufferLayout.arrayStride = 2 * sizeof(float);
         vertexBufferLayout.stepMode = wgpu::VertexStepMode::Vertex;
 
-        pipelineDesc.vertex.bufferCount = 1;
-        pipelineDesc.vertex.buffers = &vertexBufferLayout;
-
-        // NB: We define the 'shaderModule' in the second part of this chapter.
-        // Here we tell that the programmable vertex shader stage is described
-        // by the function called 'vs_main' in that module.
-        pipelineDesc.vertex.module = shaderModule;
-        pipelineDesc.vertex.entryPoint = "vs_main";
-        pipelineDesc.vertex.constantCount = 0;
-        pipelineDesc.vertex.constants = nullptr;
-
-        // Each sequence of 3 vertices is considered as a triangle
-        pipelineDesc.primitive.topology = wgpu::PrimitiveTopology::TriangleList;
-
-        // We'll see later how to specify the order in which vertices should be
-        // connected. When not specified, vertices are considered sequentially.
-        pipelineDesc.primitive.stripIndexFormat = wgpu::IndexFormat::Undefined;
-
-        // The face orientation is defined by assuming that when looking
-        // from the front of the face, its corner vertices are enumerated
-        // in the counter-clockwise (CCW) order.
-        pipelineDesc.primitive.frontFace = wgpu::FrontFace::CCW;
-
-        // But the face orientation does not matter much because we do not
-        // cull (i.e. "hide") the faces pointing away from us (which is often
-        // used for optimization).
-        pipelineDesc.primitive.cullMode = wgpu::CullMode::None;
-
-        wgpu::BlendState blendState;
+        wgpu::BlendState blendState{};
         blendState.color.srcFactor = wgpu::BlendFactor::SrcAlpha;
         blendState.color.dstFactor = wgpu::BlendFactor::OneMinusSrcAlpha;
         blendState.color.operation = wgpu::BlendOperation::Add;
@@ -83,41 +46,41 @@ namespace nugie {
         blendState.alpha.dstFactor = wgpu::BlendFactor::One;
         blendState.alpha.operation = wgpu::BlendOperation::Add;
 
-        wgpu::ColorTargetState colorTarget;
+        wgpu::ColorTargetState colorTarget{};
         colorTarget.format = device->getSurfaceFormat();
         colorTarget.blend = &blendState;
-        colorTarget.writeMask = wgpu::ColorWriteMask::All; // We could write to only some of the color channels.
+        colorTarget.writeMask = wgpu::ColorWriteMask::All;
 
-        // We tell that the programmable fragment shader stage is described
-        // by the function called 'fs_main' in the shader module.
-        wgpu::FragmentState fragmentState;
+        wgpu::FragmentState fragmentState{};
         fragmentState.module = shaderModule;
         fragmentState.entryPoint = "fs_main";
         fragmentState.constantCount = 0;
         fragmentState.constants = nullptr;
-
-        // We have only one target because our render pass has only one output color
-        // attachment.
         fragmentState.targetCount = 1;
         fragmentState.targets = &colorTarget;
 
+        wgpu::RenderPipelineDescriptor pipelineDesc{};
+        pipelineDesc.vertex.bufferCount = 1;
+        pipelineDesc.vertex.buffers = &vertexBufferLayout;
+        pipelineDesc.vertex.module = shaderModule;
+        pipelineDesc.vertex.entryPoint = "vs_main";
+        pipelineDesc.vertex.constantCount = 0;
+        pipelineDesc.vertex.constants = nullptr;
+
+        pipelineDesc.primitive.topology = wgpu::PrimitiveTopology::TriangleList;
+        pipelineDesc.primitive.stripIndexFormat = wgpu::IndexFormat::Undefined;
+        pipelineDesc.primitive.frontFace = wgpu::FrontFace::CCW;
+        pipelineDesc.primitive.cullMode = wgpu::CullMode::None;
+
         pipelineDesc.fragment = &fragmentState;
-
-        // We do not use stencil/depth testing for now
         pipelineDesc.depthStencil = nullptr;
-
-        // Samples per pixel
         pipelineDesc.multisample.count = 1;
-        // Default value for the mask, meaning "all bits on"
         pipelineDesc.multisample.mask = ~0u;
-        // Default value as well (irrelevant for count = 1 anyways)
         pipelineDesc.multisample.alphaToCoverageEnabled = false;
-
         pipelineDesc.layout = nullptr;
 
         pipeline = device->createRenderPipeline(pipelineDesc);
 
-        // We no longer need to access the shader module
         shaderModule.release();
     }
 
@@ -126,7 +89,7 @@ namespace nugie {
     }
 
     void BasicRenderSystem::render(wgpu::CommandEncoder commandEncoder, wgpu::TextureView surfaceTextureView, MeshBuffer meshBuffer) {
-        wgpu::RenderPassColorAttachment renderPassColorAttachment = {};
+        wgpu::RenderPassColorAttachment renderPassColorAttachment{};
         renderPassColorAttachment.view = surfaceTextureView;
         renderPassColorAttachment.resolveTarget = nullptr;
         renderPassColorAttachment.loadOp = wgpu::LoadOp::Clear;
@@ -137,7 +100,7 @@ namespace nugie {
         renderPassColorAttachment.depthSlice = WGPU_DEPTH_SLICE_UNDEFINED;
     #endif // NOT WEBGPU_BACKEND_WGPU
 
-        wgpu::RenderPassDescriptor renderPassDesc = {};
+        wgpu::RenderPassDescriptor renderPassDesc{};
         renderPassDesc.nextInChain = nullptr;
         renderPassDesc.colorAttachmentCount = 1;
         renderPassDesc.colorAttachments = &renderPassColorAttachment;
@@ -146,18 +109,14 @@ namespace nugie {
 
         wgpu::RenderPassEncoder renderPass = commandEncoder.beginRenderPass(renderPassDesc);
 
-        // Select which render pipeline to use
         renderPass.setPipeline(this->pipeline);
 
-        // Set vertex buffer while encoding the render pass
         BufferInfo bufferInfo = meshBuffer.vertexBuffer.getInfo();
         renderPass.setVertexBuffer(0, bufferInfo.buffer, bufferInfo.offset, bufferInfo.size);
         
-        // Set index buffer while encoding the render pass
         bufferInfo = meshBuffer.indexBuffer.getInfo();
         renderPass.setIndexBuffer(bufferInfo.buffer, wgpu::IndexFormat::Uint16, bufferInfo.offset, bufferInfo.size);
 
-        // We use the `vertexCount` variable instead of hard-coding the vertex count
         renderPass.drawIndexed(meshBuffer.indexCount, 1, 0, 0, 0);
 
         renderPass.end();
--- src/render/renderer.cpp
+++ src/render/renderer.cpp
@@ -10,15 +10,13 @@ namespace nugie {
     }
 
     void Renderer::render(Device* device) {
-        wgpu::CommandEncoderDescriptor commandEncoderDesc = {};
+        wgpu::CommandEncoderDescriptor commandEncoderDesc{};
         commandEncoderDesc.nextInChain = nullptr;
         commandEncoderDesc.label = "Frame Command Encoder";
 
-        // Get the command encoder
         wgpu::CommandEncoder commandEncoder = device->createCommandEncoder(commandEncoderDesc);
         if (!commandEncoder) return;
 
-        // Get the next target texture view
         wgpu::TextureView surfaceTextureView = device->getNextSurfaceTextureView();
         if (!surfaceTextureView) return;
 
@@ -52,41 +50,30 @@ namespace nugie {
     }
 
     void Renderer::initializeBuffers(Device* device) {
-        // Define point data
-        // The de-duplicated list of point positions
         std::vector<float> pointData = {
-            -0.5, -0.5, // Point #0 (A)
-            +0.5, -0.5, // Point #1
-            +0.5, +0.5, // Point #2 (C)
-            -0.5, +0.5, // Point #3
+            -0.5, -0.5,
+            +0.5, -0.5, 
+            +0.5, +0.5,
+            -0.5, +0.5, 
         };
 
-        // Define index data
-        // This is a list of indices referencing positions in the pointData
         std::vector<uint16_t> indexData = {
-            0, 1, 2, // Triangle #0 connects points #0, #1 and #2
-            0, 2, 3  // Triangle #1 connects points #0, #2 and #3
+            0, 1, 2,
+            0, 2, 3 
         };
 
-        // We will declare vertexCount as a member of the Application class
-        // uint32_t indexCount = static_cast<uint32_t>(indexData.size());
-
-        // Create vertex buffer
         wgpu::BufferDescriptor bufferDesc;
         bufferDesc.size = pointData.size() * sizeof(float);
-        bufferDesc.usage = wgpu::BufferUsage::CopyDst | wgpu::BufferUsage::Vertex; // Vertex usage here!
+        bufferDesc.usage = wgpu::BufferUsage::CopyDst | wgpu::BufferUsage::Vertex;
         bufferDesc.mappedAtCreation = false;
         vertexBuffer = device->createMasterBuffer(bufferDesc);
 
-        // Upload geometry data to the buffer
         device->getQueue().writeBuffer(vertexBuffer->getNative(), 0, pointData.data(), bufferDesc.size);
 
-        // Create index buffer
         bufferDesc.size = indexData.size() * sizeof(uint16_t);
-        bufferDesc.usage = wgpu::BufferUsage::CopyDst | wgpu::BufferUsage::Index; // Index usage here!
+        bufferDesc.usage = wgpu::BufferUsage::CopyDst | wgpu::BufferUsage::Index;
         indexBuffer = device->createMasterBuffer(bufferDesc);
         
-        // Upload geometry data to the buffer
         device->getQueue().writeBuffer(indexBuffer->getNative(), 0, indexData.data(), bufferDesc.size);
 
         meshBuffers.clear();
